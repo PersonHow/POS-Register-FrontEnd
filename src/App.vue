f89316435782a0584402ba6d978b3c96e28ce542
eileen171 <eileencheng.taiwan@gmail.com>
2024-07-10 16:37:48 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import OrderPage from './views/OrderPage.vue';
+import BillPage from './views/BillPage.vue';
 
 </script>
 
 <template>
 <div class="routerViewArea">
     <!-- <RouterView /> -->
-    <OrderPage />
+    <!-- <OrderPage /> -->
+     <BillPage />
 </div>
 </template>
 
