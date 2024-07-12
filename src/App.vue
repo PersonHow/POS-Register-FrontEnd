577b885f26f5db095c5e57c86b5e4cf82f7c82af
Tsai Hong-Ping <t0736828@gmail.com>
2024-07-12 11:50:53 +0800

    add routing in app.vue and index.js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+import { ref, onMounted, computed } from 'vue';
+import axios from "axios";
+const default_email="BBB@yahoo.com";
+const default_password = "123"
+onMounted(async()=>{
+    let response = await axios.post("http://localhost:8080/staff/login",{email:default_email,password:default_password});
+    sessionStorage.setItem("token",JSON.stringify(response.data));
+})
 </script>
 
 <template>
-
+    <RouterView/>
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import WorkingAreaView from '../views/WorkingAreaView.vue'
+import App from '@/App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,9 +12,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path:'/working_area',
+      name: "working_area",
+      component:WorkingAreaView
     }
   ]
 })
