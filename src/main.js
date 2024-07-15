ac56bc8da6f73efc44bd24aa3175c8fdcb80a4a7
eileen171 <eileencheng.taiwan@gmail.com>
2024-07-15 13:40:13 +0800

    update LeftNavEditOrder

--- src/main.js
+++ src/main.js
@@ -1,45 +1,26 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./assets/main.scss";
-import router from "./router";
-import { createPinia } from "pinia";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import draggable from "vuedraggable";
-// Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import './assets/main.scss'
 
-const app = createApp(App);
-const pinia = createPinia();
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import draggable from 'vuedraggable';
 
-<<<<<<<<< Temporary merge branch 1
+
 const app = createApp(App)
 const pinia = createPinia()
 
 library.add(fas, far, fab)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
+app.component('draggable', draggable);
 
 app.use(router)
 app.use(pinia)
 
 app.mount('#app')
-=========
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-app.use(router);
-app.use(pinia);
-app.use(vuetify);
-library.add(fas, far, fab);
-app.component("font-awesome-icon", FontAwesomeIcon);
-app.component("draggable", draggable);
-app.mount("#app");
->>>>>>>>> Temporary merge branch 2
--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      // path: "/",
+      // name: "home",
+      // component: HomeView,
     },
     {
-      path: "/table",
-      name: "table",
-      component: () => import("../views/TablePage.vue"),
+      // path: "/table",
+      // name: "table",
+      // component: () => import("../views/TablePage.vue"),
     },
     {
       path: "/OrderPage",
@@ -20,43 +20,33 @@ const router = createRouter({
       component: () => import("../views/OrderPage.vue"),
     },
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      // path: "/workingarea",
+      // name: "workingarea",
+      // component: () => import("../views/WorkingAreaView.vue"),
     },
     {
-      path: '/OrderPage',
-      name: 'OrderPage',
-      component: () => import('../views/OrderPage.vue')
-    },
-    {
->>>>>>>>> Temporary merge branch 2
-      path: "/BillPage",
+      path: "/BillPage/:orderId",
       name: "BillPage",
       component: () => import("../views/BillPage.vue"),
     },
     {
-      path: '/InvoicePage',
-      name: 'InvoicePage',
-      component: () => import('../views/InvoicePage.vue')
-    }
-  ]
-})
-
-export default router; // 導出路由器
-    {
-      path: "/calendar",
-      name: "calendar",
-      component: () => import("../views/Calendar.vue"),
+      path: "/InvoicePage",
+      name: "InvoicePage",
+      component: () => import("../views/InvoicePage.vue"),
     },
     {
-      path: "/logout",
-      name: "logout",
-      component: () => {
-        sessionStorage.removeItem("token");
-        alert("登出成功！");
-        window.location.replace("/");
-      },
+      // path: "/calendar",
+      // name: "calendar",
+      // component: () => import("../views/Calendar.vue"),
+    },
+    {
+      // path: "/logout",
+      // name: "logout",
+      // component: () => {
+      //   sessionStorage.removeItem("token");
+      //   alert("登出成功！");
+      //   window.location.replace("/");
+      // },
     },
   ],
 });
